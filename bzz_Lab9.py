9941d9361bc83c4d04bff3f4010c82ac8d61a807
JakeDrummond1 <jakedrummond8@gmail.com>
2024-04-10 13:18:06 -0400

    Lab9 with decoder implemented

--- bzz_Lab9.py
+++ bzz_Lab9.py
@@ -1,24 +1,44 @@
 # Bryan Zheng
+
 def main():
+    encoded_password = ""
+
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
         choice = input("Please enter an option: ")
+
         if choice == '1':
             password = input("Please enter your password to encode: ")
             encoded_password = encode(password)
+            print("Your password has been encoded and stored!\n")
+
         elif choice == '2':
-            password = input(f"The encoded password is {encoded_password}, and the original password is {}")
-            decoded_password = decode(password)
+            if encoded_password:
+                decoded_password = decode(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
+            else:
+                print("No password has been encoded yet. Please encode a password first.\n")
+
         elif choice == '3':
             break
+
+        else:
+            print("Invalid option. Please try again.\n")
 
 
 def encode(password):
     encoded_password = ""
     for digit in password:
-        encoded_digit = str((int(digit) + 3))
+        encoded_digit = str((int(digit) + 3) % 10)
         encoded_password += encoded_digit
     return encoded_password
+
+def decode(password):
+    decoded_password = ""
+    for digit in password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
 
 
 if __name__ == "__main__":
